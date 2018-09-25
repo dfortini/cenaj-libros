9c3865b7ce3b5a0bfaf6d418a5e6b6202a9e581a
Débora Fortini <deby@scvsoft.com>
2018-09-25 00:49:32 -0300

    Add React CSS Modules. Change code to use Css Modules.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import CSSModules from 'react-css-modules';
+import styles from './App.module.css';
+console.log(styles);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div styleName="App">
+        <header styleName="App-header">
+          <img src={logo} styleName="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className="App-link"
+            styleName="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
@@ -25,4 +27,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default CSSModules(App, styles);
