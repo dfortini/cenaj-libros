edde84d5158554a2524668691337dd6d8ccf9fa4
Agustín Ruiz <hola@agustinruiz.com>
2018-10-05 15:20:01 -0300

    Añadidos los archivos scss y vinculados con el .js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React, { Component } from 'react';
 import logo from './img/logo.svg';
 import CSSModules from 'react-css-modules';
-import styles from './App.module.scss';
+import styles from './index.scss';
 console.log(styles);
 
 class App extends Component {
   render() {
     return (
-      <div styleName="App">
-        <header styleName="App-header">
-          <img src={logo} styleName="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            styleName="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div styleName="mainContainer">
+        <header styleName="">
+        <h1>Hola soy la app</h1>
+        <img src={logo} styleName="" alt="logo" />
         </header>
       </div>
     );
