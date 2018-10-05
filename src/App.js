500bb73a6f964115cf45b57eb165b28c0fa51eb5
Agustín Ruiz <hola@agustinruiz.com>
2018-10-05 20:22:08 -0300

    Armado esqueleto (a revisar) de navegación principal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
-import logo from './img/logo.svg';
-import CSSModules from 'react-css-modules';
-import styles from './index.module.scss';
+import React, { Component } from "react";
+import logo from "./img/logo.svg";
+import CSSModules from "react-css-modules";
+import styles from "./index.module.scss";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 console.log(styles);
 
@@ -9,9 +10,25 @@ class App extends Component {
   render() {
     return (
       <div styleName="mainContainer">
-        <header styleName="">
-        <h1>Hola soy la app</h1>
-        <img src={logo} styleName="" alt="logo" />
+        <header>
+          <div styleName="logo">
+            <img src={logo} styleName="logoImage" alt="El Jauretche" />
+            <div styleName="logoText">
+              <p styleName="logoTitle">El Jauretche</p>
+              <p styleName="logoSubtitle">CENTRO DE ESTUDIOS NACIONALES</p>
+            </div>
+          </div>
+          <nav styleName="mainNav">
+            <Router>
+              <Link to="/" styleName="mainNavLinkActive">¿Quiénes somos?</Link>
+            </Router>
+            <Router>
+              <Link to="/" styleName="mainNavLink">Biblioteca</Link>
+            </Router>
+            <Router>
+              <Link to="/" styleName="mainNavLink">Contacto</Link>
+            </Router>
+          </nav>
         </header>
       </div>
     );
