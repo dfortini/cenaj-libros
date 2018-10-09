d4ec4b06ccfe6d08dfa00e97ea6e902828d5ea7b
Débora Fortini <deby@scvsoft.com>
2018-10-09 20:02:17 -0300

    Agregué una query al graphql que devuelve una lista harcodeada de libros.

--- server/src/index.js
+++ server/src/index.js
@@ -4,16 +4,29 @@ import graphqlHTTP from 'express-graphql';
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
+  type Libro {
+    titulo: String
+    autor: String
+  }
+
   type Query {
-    hello: String
+    libro: Libro
+    libros: [Libro]
   }
 `);
 
 // The root provides a resolver function for each API endpoint
 var root = {
-  hello: () => {
-    return 'Hello world!';
+  // TODO: buscar un libro
+  libro: () => {
+    return { titulo: "PATRIA LIBRE, JUSTA Y SOBERANA", autor: "GSPONER; PINEIRO" };
   },
+
+  libros: () => [
+    { titulo: "FUERZAS ESPIRITUALES DEL PERONISTA", autor: "PP" },
+    { titulo: "PATRIA LIBRE, JUSTA Y SOBERANA", autor: "GSPONER; PINEIRO" },
+    { titulo: "LA MUJER, EL ESTADO Y LA REVOLUCION", autor: "GOLDMAN WENDY Z." }
+  ]
 };
 
 var app = express();
