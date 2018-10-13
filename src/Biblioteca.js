f14fafd0e1ee928ab10e735b29ec9da3887e6ae5
Agustín Ruiz <hola@agustinruiz.com>
2018-10-13 12:56:52 -0300

    Agregada la grilla de libros demo. Falta agregar el form de búsqueda pero me tira muchos errores con html pelado

--- src/Biblioteca.js
+++ src/Biblioteca.js
@@ -4,18 +4,61 @@ import CSSModules from "react-css-modules";
 import "./css/_base.scss";
 import styles from "./Biblioteca.module.scss";
 
+import placeholder from "./img/book-placeholder.svg";
+
 class Biblioteca extends Component {
   render() {
     return (
-  
-        <div styleName="sectionTitle">
+      <>
+        <section styleName="sectionTitle">
           <h1>Biblioteca</h1>
           <p>
             Buscamos junto a Publicaciones del Sur recuperar escritos históricos
             y promocionar letras nacientes aportando a la cultura
             latinoamericana.
           </p>
-        </div>
+        </section>
+        <section styleName="search">
+          {/* TODO: ver como se agrega el formulario */}
+        </section>
+        <section styleName="bookList">
+          <div styleName="bookInList">
+            <img src={placeholder} styleName="bookPicture" alt="LibroNombre" />
+            <h4>¿Qué es la izquierda Nacional?</h4>
+            <p>José Luis Madariaga</p>
+          </div>
+          <div styleName="bookInList">
+            <img src={placeholder} styleName="bookPicture" alt="LibroNombre" />
+            <h4>¿Qué es la izquierda Nacional?</h4>
+            <p>José Luis Madariaga</p>
+          </div>
+          <div styleName="bookInList">
+            <img src={placeholder} styleName="bookPicture" alt="LibroNombre" />
+            <h4>¿Qué es la izquierda Nacional?</h4>
+            <p>José Luis Madariaga</p>
+          </div>
+          <div styleName="bookInList">
+            <img src={placeholder} styleName="bookPicture" alt="LibroNombre" />
+            <h4>¿Qué es la izquierda Nacional?</h4>
+            <p>José Luis Madariaga</p>
+          </div>
+          <div styleName="bookInList">
+            <img src={placeholder} styleName="bookPicture" alt="LibroNombre" />
+            <h4>¿Qué es la izquierda Nacional?</h4>
+            <p>José Luis Madariaga</p>
+          </div>
+          <div styleName="bookInList">
+            <img src={placeholder} styleName="bookPicture" alt="LibroNombre" />
+            <h4>¿Qué es la izquierda Nacional?</h4>
+            <p>José Luis Madariaga</p>
+          </div>
+          <div styleName="bookInList">
+            <img src={placeholder} styleName="bookPicture" alt="LibroNombre" />
+            <h4>¿Qué es la izquierda Nacional?</h4>
+            <p>José Luis Madariaga</p>
+          </div>
+        </section>
+      </>
     );
   }
 }
